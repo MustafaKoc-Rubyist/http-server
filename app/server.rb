567d6833f91359bb824bddb75bb4c9370f212347
OmarElsorogy <gneomar2021@gmail.com>
2025-01-01 12:41:29 +0300

    Added response and headers

--- app/server.rb
+++ app/server.rb
@@ -1,9 +1,22 @@
 require "socket"
 
-# You can use print statements as follows for debugging, they'll be visible when running tests.
-print("Logs from your program will appear here!")
-
-# Uncomment this to pass the first stage
-#
 server = TCPServer.new("localhost", 4221)
-client_socket, client_address = server.accept
+puts "Server is running on localhost:4221"
+
+loop do
+  client_socket, client_address = server.accept
+  
+
+  response = "Hello World!"
+  
+  headers = [
+    "HTTP/1.1 200 OK",
+    "Content-Type: text/plain",
+    "Content-Length: #{response.bytesize}",
+    "",
+    response
+  ].join("\r\n")
+  
+  client_socket.puts(headers)
+  client_socket.close
+end
